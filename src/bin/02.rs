78ed6006bf07744f36f5d04205aaa3682169caac
cloud303-cholden <80974257+cloud303-cholden@users.noreply.github.com>
2022-12-03 13:31:30 -0700

    #3 Solve day 2 part 2, refactor part 1

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -7,11 +7,55 @@ pub enum OpponentChoice {
     C,
 }
 
+impl OpponentChoice {
+    pub fn to_choice(self) -> Choice {
+        use Choice::*;
+        use OpponentChoice::*;
+        match self {
+            A => Rock,
+            B => Paper,
+            C => Scissors,
+        }
+    }
+}
+
 #[derive(EnumString)]
 pub enum MyChoice {
     X,
     Y,
     Z,
+}
+
+impl MyChoice {
+    pub fn to_choice(self) -> Choice {
+        use Choice::*;
+        use MyChoice::*;
+        match self {
+            X => Rock,
+            Y => Paper,
+            Z => Scissors,
+        }
+    }
+}
+
+#[derive(EnumString)]
+pub enum DesiredOutcome {
+    X,
+    Y,
+    Z,
+}
+
+impl DesiredOutcome {
+    pub fn to_outcome(self) -> Outcome {
+        use DesiredOutcome::*;
+        use Outcome::*;
+
+        match self {
+            X => Lose,
+            Y => Draw,
+            Z => Win,
+        }
+    }
 }
 
 pub enum Choice {
@@ -40,6 +84,16 @@ impl Choice {
         }
     }
 
+    pub fn to_opponent(self) -> OpponentChoice {
+        use Choice::*;
+        use OpponentChoice::*;
+        match self {
+            Rock => A,
+            Paper => B,
+            Scissors => C,
+        }
+    }
+
     pub fn my(my_choice: &MyChoice) -> Self {
         use Choice::*;
         use MyChoice::*;
@@ -47,6 +101,40 @@ impl Choice {
             X => Rock,
             Y => Paper,
             Z => Scissors,
+        }
+    }
+
+    pub fn to_my(self) -> MyChoice {
+        use Choice::*;
+        use MyChoice::*;
+        match self {
+            Rock => X,
+            Paper => Y,
+            Scissors => Z,
+        }
+    }
+
+    pub fn from_outcome(outcome: &Outcome, choice: &Choice) -> Self {
+        use Choice::*;
+        use Outcome::*;
+
+        // More questionable decisions...
+        match outcome {
+            Win => match choice {
+                Rock => Paper,
+                Paper => Scissors,
+                Scissors => Rock,
+            },
+            Draw => match choice {
+                Rock => Rock,
+                Paper => Paper,
+                Scissors => Scissors,
+            },
+            Lose => match choice {
+                Rock => Scissors,
+                Paper => Rock,
+                Scissors => Paper,
+            },
         }
     }
 }
@@ -67,11 +155,9 @@ impl Outcome {
         }
     }
 
-    pub fn new(my_choice: &MyChoice, opponent_choice: &OpponentChoice) -> Self {
+    pub fn new(my_choice: &Choice, opponent_choice: &Choice) -> Self {
         use Choice::*;
         use Outcome::*;
-        let my_choice = Choice::my(my_choice);
-        let opponent_choice = Choice::opponent(opponent_choice);
 
         // Definitely a naive approach, but it gets the job done.
         match (my_choice, opponent_choice) {
@@ -86,6 +172,16 @@ impl Outcome {
             (Paper, Paper) => Draw,
         }
     }
+
+    pub fn desired(desired_outcome: &DesiredOutcome) -> Self {
+        use DesiredOutcome::*;
+        use Outcome::*;
+        match desired_outcome {
+            X => Lose,
+            Y => Draw,
+            Z => Win,
+        }
+    }
 }
 
 fn strip_trailing_newline(input: &str) -> &str {
@@ -97,27 +193,65 @@ fn strip_trailing_newline(input: &str) -> &str {
 
 pub fn part_one(input: &str) -> Option<u32> {
     // Strip trailing newline to avoid panicking at
-    // 'called `Result::unwrap()` on an `Err` value: VariantNotFound'
+    // 'called `Result::unwrap()` on an `Err` value: VariantNotFound'.
+    // Would be better to handle the error rather than eliminate a single possible one.
     let input = strip_trailing_newline(input);
 
-    let choices: Vec<Vec<&str>> = input.split('\n').map(|i| i.split(' ').collect()).collect();
+    let choices: Vec<Vec<&str>> = input
+        .split('\n')
+        .map(|i| i.split(' ').collect())
+        .collect();
 
     let mut score = 0;
 
     for choice in choices {
-        let opponent_choice: OpponentChoice = choice[0].parse().unwrap();
-        let my_choice: MyChoice = choice[1].parse().unwrap();
+        let opponent_choice: Choice = choice[0]
+            .parse::<OpponentChoice>()
+            .unwrap()
+            .to_choice();
+        let my_choice: Choice = choice[1]
+            .parse::<MyChoice>()
+            .unwrap()
+            .to_choice();
 
         let outcome: Outcome = Outcome::new(&my_choice, &opponent_choice);
 
-        score += outcome.value() + Choice::my(&my_choice).value()
+        score += outcome.value() + my_choice.value()
     }
 
     Some(score)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    // Strip trailing newline to avoid panicking at
+    // 'called `Result::unwrap()` on an `Err` value: VariantNotFound'.
+    // Would be better to handle the error rather than eliminate a single possible one.
+    let input = strip_trailing_newline(input);
+
+    let strategies: Vec<Vec<&str>> = input
+        .split('\n')
+        .map(|i| i.split(' ').collect())
+        .collect();
+
+    let mut score = 0;
+
+    for strategy in strategies {
+        let opponent_choice: Choice = strategy[0]
+            .parse::<OpponentChoice>()
+            .unwrap()
+            .to_choice();
+        let desired_outcome: Outcome = strategy[1]
+            .parse::<DesiredOutcome>()
+            .unwrap()
+            .to_outcome();
+
+        let my_choice: Choice = Choice::from_outcome(&desired_outcome, &opponent_choice);
+        let outcome: Outcome = Outcome::new(&my_choice, &opponent_choice);
+
+        score += outcome.value() + my_choice.value()
+    }
+
+    Some(score)
 }
 
 fn main() {
@@ -140,6 +274,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(12));
     }
 }
