108b38d799d417ffeec73c0b4d6fd9b0009a22c1
cloud303-cholden <80974257+cloud303-cholden@users.noreply.github.com>
2022-12-03 11:12:03 -0700

    #1 Solve day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -26,8 +26,38 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(max)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let calories: Vec<Option<u32>> = input
+        .split('\n')
+        .map(|i| FromStr::from_str(i).ok())
+        .collect();
+
+    let mut current: u32 = 0;
+    let mut first: u32 = 0;
+    let mut second: u32 = 0;
+    let mut third: u32 = 0;
+
+    // Pretty naive solution. Might come back to this.
+    for calorie in calories {
+        let calorie = calorie.unwrap_or(0);
+        match calorie {
+            0 => {
+                if current > first {
+                    third = second;
+                    second = first;
+                    first = current
+                } else if current > second {
+                    third = second;
+                    second = current
+                } else if current > third {
+                    third = current
+                }
+                current = 0
+            }
+            _ => current += calorie,
+        }
+    }
+    Some(first + second + third)
 }
 
 fn main() {
@@ -49,6 +79,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(45000));
     }
 }
