bf6e9d5150832f7dcf24df2d26423bc7fe78276e
cloud303-cholden <80974257+cloud303-cholden@users.noreply.github.com>
2022-12-06 18:42:39 -0700

    #9 Solve day 5 part 1

---
+++ src/bin/05.rs
@@ -0,0 +1,129 @@
+use itertools::Itertools;
+
+// https://users.rust-lang.org/t/solved-how-to-split-string-into-multiple-sub-strings-with-given-length/10542/9
+// Thank you!
+pub fn sub_strings(string: String, sub_len: usize) -> Vec<String> {
+    let mut subs = Vec::with_capacity(string.len() / sub_len);
+    let mut iter = string.chars();
+    let mut pos = 0;
+
+    while pos < string.len() {
+        let mut len = 0;
+        for ch in iter.by_ref().take(sub_len) {
+            len += ch.len_utf8();
+        }
+        subs.push(string[pos..pos + len - 1].to_string());
+        pos += len;
+    }
+    subs
+}
+
+pub fn part_one(input: &str) -> Option<String> {
+    // Could definitely have done this one cleaner. But I'm slow enough already!
+
+    let (stack_rows, procedures) = input
+        .split_once("\n\n")
+        .unwrap();
+
+    // Parse stack configuration into rows of items
+    let mut stack_rows: Vec<Vec<String>> = stack_rows
+        .lines()
+        .map(|l| l.to_string())
+        .map(|mut l| {
+            l.push(' ');
+            l
+        })
+        .map(|l| sub_strings(l, 4))
+        .collect();
+
+    let stack_width = stack_rows[stack_rows.len() - 1].len();
+    // Remove stack numbers
+    stack_rows.pop();
+    stack_rows.reverse();
+    // Hack to hopefully not index out of bounds
+    let stack_height = stack_rows.len() * 2;
+
+    // Invert 2D vector so that outer dimension consists in stacks rather than rows.
+    let mut stacks: Vec<Vec<char>> = vec![vec![' '; stack_width]; stack_height];
+    for (row_index, row) in stack_rows
+        .into_iter()
+        .enumerate()
+    {
+        for (item_index, item) in row.into_iter().enumerate() {
+            let item = item.chars().nth(1).unwrap();
+            if item == ' ' {
+                continue;
+            }
+            stacks[item_index][row_index] = item;
+        }
+    }
+
+    // Filter out `' '` from stacks.
+    stacks = stacks
+        .into_iter()
+        .map(|v| {
+            v.into_iter()
+                .filter(|c| c != &' ')
+                .collect()
+        })
+        .collect();
+
+    // Build procedures
+    let procedures: Vec<(usize, usize, usize)> = procedures
+        .lines()
+        .map(|l| {
+            l.to_string()
+                .split_whitespace()
+                .filter_map(|s| s.parse::<usize>().ok())
+                .collect_tuple::<(usize, usize, usize)>()
+                .unwrap()
+        })
+        .collect();
+
+    // Apply procedures
+    for p in procedures {
+        for _ in 0..p.0 {
+            let item = stacks[p.1 - 1].pop().unwrap();
+            stacks[p.2 - 1].push(item);
+        }
+    }
+
+    // Extract the top item from each stack into a `String`
+    let top_items: String = stacks
+        .into_iter()
+        .map(|stack| match stack.last() {
+            Some(c) => c.to_string(),
+            None => "".to_owned(),
+        })
+        .into_iter()
+        .collect();
+
+    Some(top_items)
+}
+
+pub fn part_two(_input: &str) -> Option<u32> {
+    None
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 5);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 5);
+        assert_eq!(part_one(&input), Some("CMZ".to_string()));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 5);
+        assert_eq!(part_two(&input), None);
+    }
+}
